7342482c8a7810cbb9575d5c57020b94ee4ee07a
Andrew Chao <andrew.c.chao@ischool.berkeley.edu>
2012-10-30 13:49:39 -0700

    Began OO javascript
    Added a rounded corner background to the list view.
    Created a sandbox object to hold the whole app.
    List and Item objects.

--- todo.js
+++ todo.js
@@ -1,31 +1,73 @@
 
 // Handle form Submissions
 $(document).ready(function() {
+    var sandbox = new Sandbox();
+    sandbox.addList("second");
 
-    $("#todoForm").on("submit", function(event){
+    $("#addListForm").on("submit", function(event){
+        console.log("I've submitted my form");
+        //Get the value and add it to the list if not empty
+        var listName = $("#list").val();
+        addList(listName);
+        
+        //Clear the form
+        $("#list").val("");
+        return false;
+    });
 
+    $("#addItemForm").on("submit", function(event){
     	console.log("I've submitted my form");
-
         //Get the value and add it to the list if not empty
         var itemText = $("#item").val();
         addItem(itemText);
         
-
         //Clear the form
         $("#item").val("");
         return false;
     });
 });
 
-function addList(name){
+function Sandbox(){
 
+    this.currentList = "Default"
+    this.lists = [];
+    
+    this.addList = function(name){
+        if(!$.trim(name))
+            return;
+
+        var newList = new List(name)
+        this.lists.push(newList);
+        newList.render();
+
+        console.log(this.lists);
+    }
+
+    //initializing sandbox
+    this.addList("Default");
 }
 
-//Generates the HTML for the new item
-function addItem(name){
-    if(!name)
-        return;
+function List(name){
+    this.name = name;
+    this.items = [];
 
-    var domElement =  '<li><div class="item-text">' + name + '</div></li>';
-    $("#todoList").append(domElement);
+    this.addItem = function(name) {
+        //Generates the HTML for the new item
+        if(!$.trim(name))
+            return;
+
+        //Save to Local Storage
+        //localStorage.setItem("1_default", "name");
+        this.items.push(name);
+        console.log(this.items);
+
+        //Add to DOM
+        var domElement =  '<li><div class="item-text"><input type="checkbox">' + name + '<a href="#">del</a></div></li>';
+        $("#todoList").append(domElement);
+    }
+
+    this.render = function(){
+        var domElement =  '<li><div class="list-text">' + this.name + '<a href="#">del</a></div></li>';
+        $("#listOfLists").append(domElement);
+    }
 }
