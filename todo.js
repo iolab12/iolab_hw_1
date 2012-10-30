a491dca78daa6df129ff0a04e4ac6fe617ae85ab
Andrew Chao <andrew.c.chao@ischool.berkeley.edu>
2012-10-30 14:32:49 -0700

    Changed to use prototype.
    After some additional research, it seems prototyping your object methods will consume less memory and run faster (not necessarily significantly faster).
    
    Only encapsulate your methods if you need access to private members.

--- todo.js
+++ todo.js
@@ -31,43 +31,43 @@ function Sandbox(){
 
     this.currentList = "Default"
     this.lists = [];
-    
-    this.addList = function(name){
-        if(!$.trim(name))
-            return;
-
-        var newList = new List(name)
-        this.lists.push(newList);
-        newList.render();
-
-        console.log(this.lists);
-    }
-
     //initializing sandbox
     this.addList("Default");
+}
+
+Sandbox.prototype.addList = function(name){
+    if(!$.trim(name))
+        return;
+
+    var newList = new List(name)
+    this.lists.push(newList);
+    newList.render();
+
+    console.log(this.lists);
 }
 
 function List(name){
     this.name = name;
     this.items = [];
-
-    this.addItem = function(name) {
-        //Generates the HTML for the new item
-        if(!$.trim(name))
-            return;
-
-        //Save to Local Storage
-        //localStorage.setItem("1_default", "name");
-        this.items.push(name);
-        console.log(this.items);
-
-        //Add to DOM
-        var domElement =  '<li><div class="item-text"><input type="checkbox">' + name + '<a href="#">del</a></div></li>';
-        $("#todoList").append(domElement);
-    }
-
-    this.render = function(){
-        var domElement =  '<li><div class="list-text">' + this.name + '<a href="#">del</a></div></li>';
-        $("#listOfLists").append(domElement);
-    }
 }
+
+List.prototype.addItem = function(name) {
+    //Generates the HTML for the new item
+    if(!$.trim(name))
+        return;
+
+    //Save to Local Storage
+    //localStorage.setItem("1_default", "name");
+    this.items.push(name);
+    console.log(this.items);
+
+    //Add to DOM
+    var domElement =  '<li><div class="item-text"><input type="checkbox">' + name + '<a href="#">del</a></div></li>';
+    $("#todoList").append(domElement);
+}
+
+List.prototype.render = function(){
+    var domElement =  '<li><div class="list-text">' + this.name + '<a href="#">del</a></div></li>';
+    $("#listOfLists").append(domElement);
+}
+
