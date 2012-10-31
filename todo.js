02ab93f0a4a63ebb069568cf141cb9ae3c970980
Andrew Chao <andrew.c.chao@ischool.berkeley.edu>
2012-10-31 02:50:45 -0700

    Deleting Lists
    Added the ability to delete lists from the dom
    Also added attaching objects to the dom element.

--- todo.js
+++ todo.js
@@ -2,13 +2,12 @@
 // Handle form Submissions
 $(document).ready(function() {
     var sandbox = new Sandbox();
-    sandbox.addList("second");
 
     $("#addListForm").on("submit", function(event){
         console.log("I've submitted my form");
         //Get the value and add it to the list if not empty
         var listName = $("#list").val();
-        addList(listName);
+        sandbox.addList(listName);
         
         //Clear the form
         $("#list").val("");
@@ -19,33 +18,72 @@ $(document).ready(function() {
     	console.log("I've submitted my form");
         //Get the value and add it to the list if not empty
         var itemText = $("#item").val();
-        addItem(itemText);
+        sandbox.currentList.addItem(itemText);
         
         //Clear the form
         $("#item").val("");
         return false;
     });
+
+    $(document).on("click", ".deleteList", function(event){
+        //remove from the dom
+        $(this).closest("li").remove();
+
+        //remove from the sandbox
+    });
+
+    $(document).on("click", "li", function(event){
+        console.log($(this).data().name);
+    });
 });
 
+// Sandbox Class (Single instance for the site)
 function Sandbox(){
-
-    this.currentList = "Default"
+    this.currentList = "";
     this.lists = [];
     //initializing sandbox
     this.addList("Default");
 }
 
+// Add a list to the sandbox. 
 Sandbox.prototype.addList = function(name){
     if(!$.trim(name))
         return;
 
+    console.log("Adding List");
     var newList = new List(name)
-    this.lists.push(newList);
-    newList.render();
+    this.lists.push({name:name, value:newList, order:this.lists.length});
+    var domElement = newList.render();
+    $(domElement).data(newList);
+    $("#listOfLists").append(domElement);
 
-    console.log(this.lists);
+    this.setCurrentList(name);
+    // console.log(this.lists);
+
+    //TODO use $.data to tie object to dom.
 }
 
+Sandbox.prototype.deleteList = function(name){
+    console.log("delete list");
+
+}
+
+// Set the current List (fails to check against lists with the same name)
+Sandbox.prototype.setCurrentList = function(name){
+    console.log("set current list");
+    for(var i = 0; i < this.lists.length; i++){
+        if(this.lists[i].name == name)
+            this.currentList = this.lists[i].value;
+    }
+
+    console.log(this.currentList);
+}
+
+// List Class
+//  Methods:
+//      addItem
+//      deleteItem
+//      toggleState
 function List(name){
     this.name = name;
     this.items = [];
@@ -67,7 +105,7 @@ List.prototype.addItem = function(name) {
 }
 
 List.prototype.render = function(){
-    var domElement =  '<li><div class="list-text">' + this.name + '<a href="#">del</a></div></li>';
-    $("#listOfLists").append(domElement);
+    var domElement = $('<li>').append('<div class="list-text">' + this.name + '<a class="deleteList" href="#">del</a></div>');
+    return domElement;
 }
 
