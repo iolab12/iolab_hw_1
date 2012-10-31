fab63346d3f921e037b5b140d30766636859f884
Andrew Chao <andrew.c.chao@ischool.berkeley.edu>
2012-10-31 07:09:53 -0700

    Finished MultiList
    Can now create multiple lists with items in each list.
    deletable lists and items.
    
    Does NOT implement localstorage yet.
    
    Began import of unit testing.

--- todo.js
+++ todo.js
@@ -4,8 +4,6 @@ $(document).ready(function() {
     var sandbox = new Sandbox();
 
     $("#addListForm").on("submit", function(event){
-        console.log("I've submitted my form");
-        //Get the value and add it to the list if not empty
         var listName = $("#list").val();
         sandbox.addList(listName);
         
@@ -15,8 +13,6 @@ $(document).ready(function() {
     });
 
     $("#addItemForm").on("submit", function(event){
-    	console.log("I've submitted my form");
-        //Get the value and add it to the list if not empty
         var itemText = $("#item").val();
         sandbox.currentList.addItem(itemText);
         
@@ -30,11 +26,25 @@ $(document).ready(function() {
         $(this).closest("li").remove();
 
         //remove from the sandbox
+        sandbox.clearList();
     });
 
-    $(document).on("click", "li", function(event){
-        console.log($(this).data().name);
+    $(document).on("click", ".deleteItem", function(event){
+        //remove from the dom
+        $(this).closest("li").remove();
     });
+
+
+    $(document).on("click", "#listOfLists .list-text", function(event){
+        sandbox.setCurrentList($(this).data());
+    });
+
+    $(document).on("click", "#todoList .item-text", function(event){
+        console.log($(this).data().name);
+        //TODO toggle item state
+        
+    });
+
 });
 
 // Sandbox Class (Single instance for the site)
@@ -45,42 +55,59 @@ function Sandbox(){
     this.addList("Default");
 }
 
-// Add a list to the sandbox. 
+// Add a list to the sandbox.
+// Methods:
+//      addList
+//      deleteList
+//      setCurrentList
 Sandbox.prototype.addList = function(name){
     if(!$.trim(name))
         return;
 
-    console.log("Adding List");
-    var newList = new List(name)
+    var newList = new List(name);
     this.lists.push({name:name, value:newList, order:this.lists.length});
     var domElement = newList.render();
-    $(domElement).data(newList);
+    $(domElement).children(".list-text").data(newList);
     $("#listOfLists").append(domElement);
 
-    this.setCurrentList(name);
+    this.setCurrentList(newList);
     // console.log(this.lists);
 
     //TODO use $.data to tie object to dom.
 }
 
-Sandbox.prototype.deleteList = function(name){
-    console.log("delete list");
-
-}
-
 // Set the current List (fails to check against lists with the same name)
-Sandbox.prototype.setCurrentList = function(name){
+Sandbox.prototype.setCurrentList = function(list){
     console.log("set current list");
     for(var i = 0; i < this.lists.length; i++){
-        if(this.lists[i].name == name)
+        if(this.lists[i].name == list.name)
             this.currentList = this.lists[i].value;
     }
+    $("#addItemForm label").html(list.name);
+    console.log(this.currentList.name);
 
-    console.log(this.currentList);
+    this.clearList();
+    this.populateListItems();
+}
+
+// Clear the TODO box
+Sandbox.prototype.clearList = function(){
+    console.log("clear List");
+    $("#todoList").empty();
+}
+
+// Populate the TODO with the current List
+Sandbox.prototype.populateListItems = function(){
+    console.log("populate items");
+    for(var i = 0; i < this.currentList.items.length; i++){
+        var domElement = this.currentList.items[i].render();
+        console.log(domElement);
+        $("#todoList").append(domElement);
+    }
 }
 
 // List Class
-//  Methods:
+// Methods:
 //      addItem
 //      deleteItem
 //      toggleState
@@ -94,18 +121,40 @@ List.prototype.addItem = function(name) {
     if(!$.trim(name))
         return;
 
+    console.log("Add Item");
     //Save to Local Storage
     //localStorage.setItem("1_default", "name");
-    this.items.push(name);
-    console.log(this.items);
+    var newItem = new Item(name);
+    this.items.push(newItem);
 
     //Add to DOM
-    var domElement =  '<li><div class="item-text"><input type="checkbox">' + name + '<a href="#">del</a></div></li>';
+    var domElement = newItem.render();
+    $(domElement).children(".item-text").data(newItem);
     $("#todoList").append(domElement);
+    // var domElement =  '<li><div class="item-text"><input type="checkbox">' + name 
+    //     + '<a class="deleteItem" href="#">del</a></div></li>';
+    // $("#todoList").append(domElement);
 }
 
+
+// List.prototype.deleteItem =  function(element){
+//     // $(element).remove();
+// }
+
 List.prototype.render = function(){
-    var domElement = $('<li>').append('<div class="list-text">' + this.name + '<a class="deleteList" href="#">del</a></div>');
+    var domElement = $('<li>').append('<div class="list-text">' + this.name 
+        + '</div><a class="deleteList" href="#">del</a>');
     return domElement;
 }
 
+
+function Item(name){
+    this.name = name;
+    this.completed = false;
+}
+
+Item.prototype.render = function(){
+    var domElement =  $('<li>').append('<div class="item-text"><input type="checkbox">' + this.name 
+        + '</div><a class="deleteItem" href="#">del</a>');
+    return domElement;
+}
